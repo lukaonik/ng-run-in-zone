cc90daa733e400e71c16cd268923569897372473
lukaonik <lukaonik@gmail.com>
2022-07-13 21:15:08 +0400

    initialized workspace

---
+++ apps/demo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-run-in-zone-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'demo';
+}
